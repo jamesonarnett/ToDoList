a3fbf0e49545191316b4be2716aaa55c769eedcf
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2021-11-08 10:39:19 -0600

    Add files via upload
    Refactor. Move date to module.

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js')
+
 
 const app = express();
 
-let items = ["Add your items!"];
-let workItems = ["Work work work"];
+const items = ["Add your items!"];
+const workItems = ["Work work work"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("Public"));
 
-app.get("/", function(req, res) {
-
-   let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-
-    let day = today.toLocaleDateString("en-US", options);
-  
+app.get("/", function(req, res) {  
+   let day = date.getDate();
     res.render('list', {listTitle: day, newListitems: items});
 
 })
